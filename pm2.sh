eaa46771393565ff4899c56682519a42620809ec
JERLINJO <joyjerlin@gmail.com>
2023-02-08 15:51:24 +0530

    eighth commit

---
+++ pm2.sh
@@ -0,0 +1,11 @@
+echo "enter 2 nos"
+read a
+read b
+s=`expr $a + $b`
+echo "sum is $s"
+m=`expr $a - $b`
+echo "Difference is $m"
+p=`expr $a \* $b`
+echo "product is $p"
+d=`expr $a / $b`
+echo "Division is $d" 
